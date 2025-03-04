a883948a81b26271596698ac98a502af3af992e2
Ing. Peter Gnip - GNIPET <gnipet@gnipet.sk>
2025-03-04 11:55:04 +0100

    Pridal som veľa komentárov na playfair.c

--- playfair.c
+++ playfair.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "bmp.h"
-#include "playfair.h"
 
+// Znaky, ktoré sa môžu používať
+#define ALPHA "ABCDEFGHIJKLMNOPQRSTUVXYZ"
 
 // Veľkosť matice
 #define MATRIX_SIZE 5
@@ -15,32 +15,33 @@ char* playfair_encrypt(const char* key, const char* text) {
 
     // Vytvorenie matice 5x5
     char matrix[MATRIX_SIZE][MATRIX_SIZE];
-    int used[26] = {0};  // Pole na sledovanie použitých písmen
+    int used[26] = {0};  // Pole na sledovanie použitých písmen, každý jeden prvok je písmeno, začíname A a končíme Z
     int index = 0;       // Index pre naplnenie matice
 
-    // Naplnenie matice kľúčom
-    for (size_t i = 0; key[i] != '\0'; i++) {
-        char c = toupper(key[i]);
-        if (c == ' ') continue;  // Ignorovať medzery
-        if (c == 'W') c = 'V';   // Nahradiť 'W' za 'V'
-        if (used[c - 'A'] == 0) {  // Ak písmeno ešte nebolo použité
-            matrix[index / MATRIX_SIZE][index % MATRIX_SIZE] = c;
-            used[c - 'A'] = 1;
-            index++;
+    // Naplnenie matice "matrix" kľúčom
+    // index je v podstate číslo (je to počet prvkov poľa) a pomocou neho napľňame maticu 5x5, Ak požijeme index/5 je to definícia riadku, ak index%5 je to stľpec
+    for (int i = 0; key[i] != '\0'; i++) {  // Prechádzame celý kľúč, až do konca 
+        char c = toupper(key[i]);            // Zmeníme na veľké písmeno
+        if (c == ' ') continue;  // Ak je medzera, preskočíme ju
+        if (c == 'W') c = 'V';   // Ak je tam 'W' zmeníme ho na 'V' - podmienka úlohy
+        if (used[c - 'A'] == 0) {  // Ak písmeno ešte nebolo použité, index pola used je presne číslo daného písmena v abecede (preto -A aby sme sa dostali na pozíciu)
+            matrix[index / MATRIX_SIZE][index % MATRIX_SIZE] = c;   // Vložíme do matice znak
+            used[c - 'A'] = 1;  // Označíme v used, že už bolo písmeno použité
+            index++;            // Na ďalší znak
         }
     }
 
-    // Doplň zvyšok abecedy (bez 'W') pomocou ALPHA
-    for (size_t i = 0; ALPHA[i] != '\0'; i++) {
-        char c = ALPHA[i];
-        if (used[c - 'A'] == 0) {
-            matrix[index / MATRIX_SIZE][index % MATRIX_SIZE] = c;
-            index++;
+    // Doplň zvyšok abecedy (bez 'W') pomocou ALPHA v ALPHA je 25 znakov teda 5x5 matica
+    for (int i = 0; ALPHA[i] != '\0'; i++) {
+        char c = ALPHA[i];          // Zoberieme písmeno 
+        if (used[c - 'A'] == 0) {   // Ak nebolo pouižité
+            matrix[index / MATRIX_SIZE][index % MATRIX_SIZE] = c; // Vložíme do matice, všímame si, že používam,e stálke rovnaký "index", teda pokračujeme v naplnení matice 5x5
+            index++;    // Ideme na ďalší prvok matice 5x5
         }
     }
 
     // Úprava textu pred šifrovaním
-    //char* prepared_text = strdup(text);
+    // char* prepared_text = strdup(text);  // Skopírujeme si TEXT
     
     char* prepared_text= malloc(strlen(text) + 1);
 
@@ -48,78 +49,79 @@ char* playfair_encrypt(const char* key, const char* text) {
         strcpy(prepared_text, text);
     }
     
-
-    size_t len = strlen(prepared_text);
-    size_t j = 0;
+    int len = strlen(prepared_text);    // Zistíme jeho dľžku
+    int j = 0;
 
     // Odstránenie medzier a konverzia na veľké písmená
-    for (size_t i = 0; i < len; i++) {
-        if (prepared_text[i] != ' ') {
-            prepared_text[j++] = toupper(prepared_text[i]);
-        }
-    }
+    for (int i = 0; i < len; i++) {       // Prechádzame TEXT
+        if (prepared_text[i] != ' ') {      // Ak je tam niej emedzera 
+            prepared_text[j++] = toupper(prepared_text[i]); // Zmeň na veľké
+        }       // Ak tam nie je medzera ide "j" a "i" v jednom súbehu ale ak je tam medzera "i" sa zvyšuje ale "j" ostáva prepared_text[j++] najprv pole[j]  potom j=j+1
+    }       // Ak je tam medzera ideme na ďalší znak 
     prepared_text[j] = '\0';
 
     // Pridanie 'X' medzi rovnaké znaky a na koniec, ak je dĺžka nepárna
+    // Ak pridávame môže sa stať že sa mení prebežne dľžka aj to či je TEXT párny/nepárny
     len = strlen(prepared_text);
-    for (size_t i = 0; i < len; i += 2) {
-        if (i + 1 < len && prepared_text[i] == prepared_text[i + 1]) {
-            memmove(&prepared_text[i + 1], &prepared_text[i], len - i);
-            prepared_text[i + 1] = 'X';
-            len++;
-            prepared_text[len] = '\0';
+    for (int i = 0; i < len; i += 2) { // Robíme po dvojiciach 
+        if (i + 1 < len && prepared_text[i] == prepared_text[i + 1]) { // Ak nerobíme poslednú dvojicu A ak je aktuálny aj nasledujúci znak rovnaký
+            memmove(&prepared_text[i + 1], &prepared_text[i], len - i); // Tento zápis vkladá nový znak do reťazca na pozícii i tak, že posunie všetky znaky doprava
+            prepared_text[i + 1] = 'X'; // Keď sme všetko posunuli o znak tak pridáme "X"
+            len++; // Pridali sme znak "X" treba zvýsiť dľžku TEXTu
+            prepared_text[len] = '\0';  // Na koniec pridaj ukončenie 
         }
     }
-    if (len % 2 != 0) {
-        prepared_text[len] = 'X';
-        prepared_text[len + 1] = '\0';
+    if (len % 2 != 0) {     // Ak je nepárny počet upravených znakov
+        prepared_text[len] = 'X';   // Pridaj na koniec "X"  
+        prepared_text[len + 1] = '\0';  // A ukončovací znak
     }
 
     // Šifrovanie textu
-    char* encrypted = (char*)malloc(len + 1);
+    char* encrypted = (char*)malloc(len + 1); // Príprava
     if (encrypted == NULL) {
         free(prepared_text);
         return NULL;
     }
 
-    for (size_t i = 0; i < len; i += 2) {
-        char a = prepared_text[i];
-        char b = prepared_text[i + 1];
+    for (int i = 0; i < len; i += 2) { // Robíme po dvojiciach 
+        char a = prepared_text[i]; // Prvý znak dvojice je "a"
+        char b = prepared_text[i + 1];  // Druhý znak dvojice je "b"
 
         // Nájdenie pozície znakov v matici
-        int row_a, col_a, row_b, col_b;
-        for (int r = 0; r < MATRIX_SIZE; r++) {
-            for (int c = 0; c < MATRIX_SIZE; c++) {
-                if (matrix[r][c] == a) {
-                    row_a = r;
+        int row_a, col_a, row_b, col_b;   // Deklarácia premenných 
+        // Prechádzame maticou a hľadáme pozície dvojice
+        for (int r = 0; r < MATRIX_SIZE; r++) {          // Riadky
+            for (int c = 0; c < MATRIX_SIZE; c++) {     // Stľpce
+                if (matrix[r][c] == a) {    // Našli sme prvý znak
+                    row_a = r;              // Uložíme pozície prvého znaku
                     col_a = c;
                 }
-                if (matrix[r][c] == b) {
-                    row_b = r;
+                if (matrix[r][c] == b) {    // Našli sme druhý znak
+                    row_b = r;              // Uložíme pozície druhého znaku
                     col_b = c;
                 }
             }
         }
 
         // Šifrovanie dvojice znakov
-        if (row_a == row_b) {  // Rovnaký riadok
+        if (row_a == row_b) {  // Rovnaký riadok = berieme nasledujúcu pozíciu v riadku
             encrypted[i] = matrix[row_a][(col_a + 1) % MATRIX_SIZE];
             encrypted[i + 1] = matrix[row_b][(col_b + 1) % MATRIX_SIZE];
-        } else if (col_a == col_b) {  // Rovnaký stĺpec
+        } else if (col_a == col_b) {  // Rovnaký stĺpec = berieme pozíciu nižsie v stľpci
             encrypted[i] = matrix[(row_a + 1) % MATRIX_SIZE][col_a];
             encrypted[i + 1] = matrix[(row_b + 1) % MATRIX_SIZE][col_b];
-        } else {  // Rôzne riadky a stĺpce
-            encrypted[i] = matrix[row_a][col_b];
-            encrypted[i + 1] = matrix[row_b][col_a];
+        } else {  // Rôzne riadky a stĺpce  
+            encrypted[i] = matrix[row_a][col_b];    // Prvý znak riadok ostáva , stľpec sa berie podľa druhého znaku
+            encrypted[i + 1] = matrix[row_b][col_a];    // Druhý znak to isté
         }
     }
-    encrypted[len] = '\0';
+    encrypted[len] = '\0';   // doplníme ukončovací znak 
 
-    free(prepared_text);
-    return encrypted;
+    free(prepared_text);    // uvoľníme pamäť
+    return encrypted;       // návrat zašifrovaného textu
 }
 
-// Funkcia na dešifrovanie textu
+// Funkcia na dešifrovanie textu porebieha opačne ako šifrovanie :-)
 char* playfair_decrypt(const char* key, const char* text) {
     if (key == NULL || text == NULL) return NULL;
 
@@ -129,7 +131,7 @@ char* playfair_decrypt(const char* key, const char* text) {
     int index = 0;       // Index pre naplnenie matice
 
     // Naplnenie matice kľúčom
-    for (size_t i = 0; key[i] != '\0'; i++) {
+    for (int i = 0; key[i] != '\0'; i++) {
         char c = toupper(key[i]);
         if (c == ' ') continue;  // Ignorovať medzery
         if (c == 'W') c = 'V';   // Nahradiť 'W' za 'V'
@@ -141,7 +143,7 @@ char* playfair_decrypt(const char* key, const char* text) {
     }
 
     // Doplň zvyšok abecedy (bez 'W') pomocou ALPHA
-    for (size_t i = 0; ALPHA[i] != '\0'; i++) {
+    for (int i = 0; ALPHA[i] != '\0'; i++) {
         char c = ALPHA[i];
         if (used[c - 'A'] == 0) {
             matrix[index / MATRIX_SIZE][index % MATRIX_SIZE] = c;
@@ -155,13 +157,13 @@ char* playfair_decrypt(const char* key, const char* text) {
     }
 
     // Dešifrovanie textu
-    size_t len = strlen(text);
+    int len = strlen(text);
     char* decrypted = (char*)malloc(len + 1);
     if (decrypted == NULL) {
         return NULL;
     }
 
-    for (size_t i = 0; i < len; i += 2) {
+    for (int i = 0; i < len; i += 2) {
         char a = text[i];
         char b = text[i + 1];
 
